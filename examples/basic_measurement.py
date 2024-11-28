ad353382b8be55fe0b0ae168e336b0cd8061b618
HarmonyOS <HarmonyOS@qq.com>
2024-11-28 21:02:36 +0800

    修改测试py

--- examples/basic_measurement.py
+++ examples/basic_measurement.py
@@ -8,11 +8,17 @@ https://github.com/mcauser/micropython-am2320
 Prints the temperature and humidity
 """
 import sys
+import time
 
 sys.path.append("..")
 from src import am2320
 
 sensor = am2320.AM2320()
 
-print(f"Temperature: {sensor.ReadTemperature()} C")
-print(f"Humidity: {sensor.ReadHumidity()} RH")
+for x in range(0, 10):
+   print(f"Temperature: {sensor.ReadTemperature()} C")
+   print(f"Humidity: {sensor.ReadHumidity()} RH")
+   for x in sensor.ReadTemperatureHumidity():
+      print(x)
+   print("\n")
+   time.sleep(0.5)
